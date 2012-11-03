6cdbae749df6abdd069c9f5cfa7a382b1b086022
Ryan Ramage <ryan.ramage@gmail.com>
2012-11-02 18:32:11 -0600

    If there is no title specified, use the key name.

--- src/json.edit.js
+++ src/json.edit.js
@@ -1004,12 +1004,13 @@
             input = priv.input(fid, type, inputId, opts, required, util),
             result;
 
+        var label_text = opts.title || fid;
         result = {
             "div": {
                 "id": id,
                 "class": priv.genFieldClasses(fid, opts, " ", required),
                 "$childs": [
-                    priv.label(opts.title, inputId),
+                    priv.label(label_text, inputId),
                     input
                 ]
             }
